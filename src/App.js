da8d059a2f3b027c5d0e1e35fda09e96d7ac9465
Sol Delgadillo <soledelgadillo@gmail.com>
2021-10-04 11:50:43 +0100

    Updated footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Sol Delgadillo and is{" "}
+        <a
+          href="https://github.com/soledelgadillo/react-weather-app"
+          target="_blank"
+        >
+          open-sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
