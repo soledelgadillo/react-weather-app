356254e7be8293a6ca449d08833e79518a9a8de4
Sol Delgadillo <soledelgadillo@gmail.com>
2021-10-04 12:04:25 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Sol Delgadillo and is{" "}
-        <a
-          href="https://github.com/soledelgadillo/react-weather-app"
-          target="_blank"
-        >
-          open-sourced
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project was coded by Sol Delgadillo and is{" "}
+          <a
+            href="https://github.com/soledelgadillo/react-weather-app"
+            target="_blank"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
