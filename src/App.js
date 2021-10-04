7bc45e03e571a1fa3128cb904412d3d04f184755
Sol Delgadillo <soledelgadillo@gmail.com>
2021-10-04 15:12:47 +0100

    Created general HTML structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        Weather App
+        <Weather />
         <footer>
           This project was coded by Sol Delgadillo and is{" "}
           <a
